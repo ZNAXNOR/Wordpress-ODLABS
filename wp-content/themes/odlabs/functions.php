066551378bafce56131ad47b6ad0efb22ef37aed
Omkar Dalvi <121810601+ZNAXNOR@users.noreply.github.com>
2025-07-14 22:38:27 +0530

    Created outline for a custom wordpress theme

---
+++ wp-content/themes/odlabs/functions.php
@@ -0,0 +1,6 @@
+<?php
+function enqueue_tailwind() {
+    wp_enqueue_style{'tailwind-css', get-template_directory_url() . './src/output.css'};
+}
+
+add_action('wp_enqueue_scripts', 'enqueue_tailwind');
